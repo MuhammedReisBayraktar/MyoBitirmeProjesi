b4dfb83105c0f621c2ef4f236d297184f5cfc4c4
Muhammed Reis <muhammed.bayraktar@arniva.com.tr>
2024-02-28 16:48:39 +0300

    Açıklama

--- Test Proje/Form1.vb
+++ Test Proje/Form1.vb
@@ -4,4 +4,9 @@
         '12345'
         'Yalnışlıkla Sildim Şimdi Kaydet'
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Test Mesaj")
+
+    End Sub
 End Class
